e0b7b975634c418b7009c15b1510ad1a1a24dd34
ZnoGouDj <znogoud3@gmail.com>
2021-12-07 19:30:09 +0300

    Add the store

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux';
 import App from './App';
+import { rootReducer } from './redux/rootReducer';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(rootReducer);
 
 render(
   <React.StrictMode>
