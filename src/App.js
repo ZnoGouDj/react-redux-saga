2e16ee55221809aa478160172d690def9d90f990
ZnoGouDj <znogoud3@gmail.com>
2021-12-07 19:50:48 +0300

    Add connect to the Posts component

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
       <div className="row">
         <div className="col">
           <h2>Sync Posts</h2>
-          <Posts posts={[]} />
+          <Posts />
         </div>
         <div className="col">
           <h2>Async Posts</h2>
