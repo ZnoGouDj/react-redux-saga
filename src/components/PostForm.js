73871dfc1bf12eb1b2ca5cb90e98935968f471e8
ZnoGouDj <znogoud3@gmail.com>
2021-12-08 23:41:13 +0300

    Add the warning message, add the removal timer

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { createPost } from './../redux/actions';
+import { createPost, showAlert } from './../redux/actions';
+import { Alert } from './Alert';
 
 class PostForm extends React.Component {
   constructor(props) {
@@ -17,7 +18,7 @@ class PostForm extends React.Component {
     const { title } = this.state;
 
     if (!title.trim()) {
-      return;
+      return this.props.showAlert('Please, enter the post name');
     }
 
     const newPost = {
@@ -42,6 +43,7 @@ class PostForm extends React.Component {
   render() {
     return (
       <form onSubmit={this.submitHandler}>
+        {this.props.alert && <Alert text={this.props.alert} />}
         <div className="form-group">
           <label htmlFor="title">Post header</label>
           <input
@@ -63,6 +65,11 @@ class PostForm extends React.Component {
 
 const mapDispatchToProps = {
   createPost,
+  showAlert,
 };
 
-export default connect(null, mapDispatchToProps)(PostForm);
+const mapStateToProps = state => ({
+  alert: state.app.alert,
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(PostForm);
