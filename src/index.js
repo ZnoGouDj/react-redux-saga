2f676b269f93c9cc0611f29edbffdeb5c4d05687
ZnoGouDj <znogoud3@gmail.com>
2021-12-09 00:04:49 +0300

    Add side effects

--- src/index.js
+++ src/index.js
@@ -2,19 +2,25 @@ import React from 'react';
 import { render } from 'react-dom';
 import { compose, createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
+import createSagaMiddleware from '@redux-saga/core';
 import App from './App';
 import { rootReducer } from './redux/rootReducer';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { forbiddenWordsMiddleware } from './redux/middleware';
+import { sagaWatcher } from './redux/sagas';
+
+const saga = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   compose(
-    applyMiddleware(thunk, forbiddenWordsMiddleware),
+    applyMiddleware(thunk, forbiddenWordsMiddleware, saga),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
 );
+
+saga.run(sagaWatcher);
 
 const app = (
   <Provider store={store}>
