74f58dfc84302e6d719cfd540388d405f6db9065
ZnoGouDj <znogoud3@gmail.com>
2021-12-08 23:29:19 +0300

    Add Loader, add fake delay to see the loader lol

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchPosts } from '../redux/actions';
+import { Loader } from './Loader';
 import Post from './Post';
 
 export default () => {
   const dispatch = useDispatch();
   const posts = useSelector(state => state.posts.fetchedPosts);
+  const loading = useSelector(state => state.app.loading);
+
+  if (loading) {
+    return <Loader />;
+  }
 
   if (!posts.length) {
     return (
